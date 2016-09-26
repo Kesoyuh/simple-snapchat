5d0b6a49945752255aec5c26cb9950ebf6dccf12
Changchang Wang <Jeffreywang1943@gmail.com>
2016-09-26 16:48:29 +1000

    test

---
+++ simple-snapchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-snapchat
+//
+//  Created by Jeffrey on 26/9/16.
+//  Copyright © 2016 University of Melbourne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
