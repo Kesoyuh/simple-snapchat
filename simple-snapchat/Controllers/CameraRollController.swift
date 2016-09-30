81d74c0668047586abc64dffab126b465a328d30
Changchang Wang <Jeffreywang1943@gmail.com>
2016-09-30 21:06:38 +1000

    finish camera roll with animation

--- simple-snapchat/Controllers/CameraRollController.swift
+++ simple-snapchat/Controllers/CameraRollController.swift
@@ -11,12 +11,16 @@ import Photos
 
 private let reuseIdentifier = "Cell"
 
-class CameraRollController: UICollectionViewController {
+class CameraRollController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Memories"
-        
+        navigationController?.navigationBar.isHidden = false
+        grabPhotos()
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView?.allowsMultipleSelection = true
+
     }
     
     var imgArray = [UIImage]()
@@ -55,14 +59,124 @@ class CameraRollController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        
-        cell.addSubview(<#T##view: UIView##UIView#>)
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoCell
+        cell.imageView.image = imgArray[indexPath.row]
+
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width/3 - 2
+        return CGSize(width: width, height: width)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2.0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2.0
+    }
+    
+//    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let selectedCell = collectionView.cellForItem(at: indexPath) as? PhotoCell
+//        selectedCell?.imageView.alpha = 0.5
+//
+//    }
+//    
+//    override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+//        let selectedCell = collectionView.cellForItem(at: indexPath) as? PhotoCell
+//        selectedCell?.imageView.alpha = 1
+//    }
+    
+}
 
-
-
+class PhotoCell: UICollectionViewCell {
+    lazy var imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.isUserInteractionEnabled = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleZoomTap)))
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+        setupImageView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupImageView() {
+        
+        imageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        
+    }
+    
+    var startingFrame: CGRect?
+    var blackBackgroundView: UIView?
+    var startingImageView: UIImageView?
+    
+    func handleZoomTap(tapGesture: UITapGestureRecognizer) {
+        if let imageView = tapGesture.view as? UIImageView {
+            
+            startingImageView = imageView
+            startingImageView?.isHidden = true
+            startingFrame = imageView.superview?.convert(imageView.frame, to: nil)
+            let zoomingImageView = UIImageView(frame: startingFrame!)
+            zoomingImageView.contentMode = .scaleAspectFill
+            zoomingImageView.clipsToBounds = true
+            zoomingImageView.image = self.startingImageView?.image
+            zoomingImageView.isUserInteractionEnabled = true
+            zoomingImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleZoomOut)))
+            
+            if let keyWindow = UIApplication.shared.keyWindow {
+                
+                blackBackgroundView = UIView(frame: keyWindow.frame)
+                blackBackgroundView?.backgroundColor = UIColor.black
+                blackBackgroundView?.alpha = 0
+                keyWindow.addSubview(blackBackgroundView!)
+                keyWindow.addSubview(zoomingImageView)
+                
+                UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    
+                    self.blackBackgroundView?.alpha = 1.0
+                    
+                    let imageWidth: CGFloat? = self.startingImageView?.image?.size.width
+                    let imageHeight: CGFloat? = self.startingImageView?.image?.size.height
+                    
+                    let height = imageHeight! / imageWidth! * keyWindow.frame.width
+                    
+                    zoomingImageView.frame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
+                    zoomingImageView.center = keyWindow.center
+                    }, completion: nil)
+            }
+            
+            
+        }
+    }
+    
+    func handleZoomOut(tapGesture: UITapGestureRecognizer) {
+        if let zoomOutImageView = tapGesture.view as? UIImageView {
+            zoomOutImageView.contentMode = .scaleAspectFill
+            zoomOutImageView.clipsToBounds = true
+            UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
+                zoomOutImageView.frame = self.startingFrame!
+                self.blackBackgroundView?.alpha = 0
+                
+                }, completion: { (completed) in
+                    zoomOutImageView.removeFromSuperview()
+                    self.startingImageView?.isHidden = false
+            })
+        }
+    }
+    
     
 }
