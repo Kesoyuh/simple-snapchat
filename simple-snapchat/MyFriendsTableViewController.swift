3beab2d61b78e417998c48daba2531ef7a4a5f67
Hailun Tian <helen.tian.93@gmail.com>
2016-10-06 22:00:22 +1100

    Finish my friends view, can show the friend list and start a new chat now

--- simple-snapchat/MyFriendsTableViewController.swift
+++ simple-snapchat/MyFriendsTableViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 import Firebase
 class MyFriendsTableViewController: UITableViewController {
     
-   
-    var friends = [User]()
-    var filterFriends = [User]()
-   // let searchController = UISearchController(searchResultsController: nil)
+   let cellID = "MyFriendCell"
+   var friends = [User]()
+   let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(ChatCell.self, forCellReuseIdentifier: cellID)
+        
         fetchFriends()
+      
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -29,7 +33,7 @@ class MyFriendsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -47,30 +51,34 @@ class MyFriendsTableViewController: UITableViewController {
     
    
     func fetchFriends(){
-        
-        self.friends = [User]()
-        FIRDatabase.database().reference().child("users").observe(.childAdded, with: {(snapshot) in
-            if let dictionary = snapshot.value as? [String: AnyObject]{
-                let user = User()
-                user.setValuesForKeys(dictionary)
-                user.id = snapshot.key
-                self.friends.append(user)
-                DispatchQueue.global().async {
-                    
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                }
-                
-            }
-            
-            
-            }, withCancel: nil)
+
+        if let myID = FIRAuth.auth()?.currentUser?.uid{
+            let friendRef = FIRDatabase.database().reference().child("friendship").child(myID)
+            friendRef.observe(.childAdded, with: { (snapshot) in
+                if snapshot.value as? Int == 2 {
+                    let key = snapshot.key
+                    let user = User()
+                    let userRef = FIRDatabase.database().reference().child("users").child(key)
+                    userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+                        if let dictionary = snapshot.value as? [String: AnyObject]{
+                            user.setValuesForKeys(dictionary)
+                            user.id = key
+                            self.friends.append(user)}
+
+                        DispatchQueue.global().async {
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    })
+                }  }, withCancel: nil)
+        }
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "MyFriends")
+       let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellID)
 
         // Configure the cell...
         let user : User
@@ -81,11 +89,18 @@ class MyFriendsTableViewController: UITableViewController {
 
         return cell
     }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let chatPartnerId = friends[indexPath.row].id{
+
+            let chatLogController = ChatLogController(collectionViewLayout: UICollectionViewFlowLayout())
+            navigationController?.pushViewController(chatLogController, animated: true)
+            chatLogController.partnerId = chatPartnerId
+        }
     
 
-    
 
-    
-    
+    }
 
 }
+
